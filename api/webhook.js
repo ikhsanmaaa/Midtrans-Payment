8f55d2b0bfcacd89b360afe919302b1562f7530a
ikhsanmaaa <ikhsanmaaa@gmail.com>
2026-02-27 17:17:24 +0700

    add bearer auth for supabase

--- api/webhook.js
+++ api/webhook.js
@@ -31,12 +31,16 @@ export default async function handler(req, res) {
 
     console.log("Webhook verified:", order_id);
 
-    if (order_id.startsWith("CAFEAPP-")) {
-      await axios.post(
-        "https://gxdtcyyjkqltlnwjzncl.supabase.co/functions/v1/midtrans-webhook",
-        body,
-      );
-    }
+    await axios.post(
+      "https://gxdtcyyjkqltlnwjzncl.supabase.co/functions/v1/midtrans-webhook",
+      body,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.SUPABASE_SECRET_KEY}`,
+          "Content-Type": "application/json",
+        },
+      },
+    );
 
     if (order_id.startsWith("MERN-")) {
       await axios.post(
